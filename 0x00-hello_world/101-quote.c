5c22ce8d1edee057ba02734bef5c8902b390e714
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-09 04:43:28 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,14 +7,13 @@
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to standard error.
+ *               followed by a new line, to standard error.
  *
  * Return: Always 1.
  */
 int main(void)
 {
-	writef(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
