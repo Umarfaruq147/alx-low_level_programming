14f07f8496f830d915aaae6c9be81c8b63275267
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-08 17:32:09 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,9 +13,7 @@
  */
 int main(void)
 {
-	fwrite(2,
-				      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-				      	      59);
+	writef(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 
 	return (1);
