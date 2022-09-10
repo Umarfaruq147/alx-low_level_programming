772dae2cccb400bdf5612e246fb6fd16c5a2e857
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-10 08:56:04 -0700

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char alphabets;
 
-	for (alphabets = 'a'; alphabets <= 'z'; alphabts++)
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 		putchar(alphabets);
 
 	putchar('\n');
