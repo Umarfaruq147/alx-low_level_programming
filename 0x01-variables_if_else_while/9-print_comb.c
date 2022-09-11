98adfdce79531252f0d05326f8021d1157793a2c
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-11 04:44:49 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,10 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		{
 		if (num != '9')
+		{
 			putchar(',');
-		putchar (' ')
-			putchar(' ');
+		putchar (' ');
 		}
 	}
 	putchar('\n');
