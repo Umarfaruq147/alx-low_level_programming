bc1a21680a2686381d787eba52a049fd54e0f095
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-11 06:12:35 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible combinations of two different digits,
- * in ascending order, seperated bu a comma followed by a space.
+ * in ascending order, seperated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -12,9 +12,9 @@ int main(void)
 
 	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = num1 +1; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			for (num3 = num2 + 2; num3 < 10; num3++)
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
