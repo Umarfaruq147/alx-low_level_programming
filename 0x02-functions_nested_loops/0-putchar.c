bbf455028899634c2a518ad34fe3a6b5fcf4eb12
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-13 06:39:25 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
-#include "Holberton"
-
+#include "_putchar"
 /**
- * main - print Holberton
+ * main - prints _putchar
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
