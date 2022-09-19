a10b4b72fb87a142a1f2d741efb04309c7f44c71
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-19 14:31:39 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other cahracterof a string
+ *
+ * @str: char to check
+ * Umarfaruq - Author
+ * Return: 0 is success
+ */
+
+void puts2(cahr *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+		if (string % 2 ==0)
+			putchar(str[string]);
+	putchar('\n');
+}
