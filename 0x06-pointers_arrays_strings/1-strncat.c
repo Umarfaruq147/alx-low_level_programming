ea7474f04a0dbb4e72e72512346c47cae76f257a
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-22 04:54:08 -0700

    fix strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	while ((src + j < src + n) && *(src + j) != '\0')
 	{
-		*(dest + i) = (*src + j);
+		*(dest + i) = *(src + j);
 		i++;
 		j++;
 	}
