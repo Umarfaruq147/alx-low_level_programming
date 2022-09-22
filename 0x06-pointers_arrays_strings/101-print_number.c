ea0396b416072057e6c7342b77e110b1eb478d21
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-22 15:53:07 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,13 +12,14 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n *= -1;
 	}
+
 	x = n;
 
 	if (x / 10)
 		print_number(x / 10);
 
-	putchar(x % 10 + '0');
+	_putchar(x % 10 + '0');
 }
