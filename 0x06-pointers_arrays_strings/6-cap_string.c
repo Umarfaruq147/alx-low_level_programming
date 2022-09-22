6b234510a90bc7473c353bf88ca38c7ab12c1910
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-22 15:22:46 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * cap_string - ...
- * @s: ...
- *
- * Return: char value
+ * cap_string - capitalize all words in a string
+ * @s: string
+ * Retuurn: address of s
  */
 char *cap_string(char *s)
 {
-	int a = 0, i;
-	int cspc = 13;
-	char spc[] = {32, '\t', '\n', 44, ';' 46, '!', '?', '"', '(', ')', '{', '}',};
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	while (s[a])
+	while (*(s + i))
 	{
-		i = 0;
-		while (i < cspc)
+		if (*(s + i) >= 'a' && *(s +i) <= 'z')
 		{
-			if ((a == 0 || s[a - 1] == spec[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] -= 32;
-			i++;
+			if (i ==0)
+				*(s +i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i -1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
-		a++;
+		i++
 	}
 	return (s);
 }
