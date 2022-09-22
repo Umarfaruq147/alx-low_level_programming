84111fd9444e02415dc80b7d94bc0d93fd51ca7d
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-22 09:27:49 -0700

    fix rev str

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@ void reverse_array(int *a, int n)
 	int tmp, index;
 	for (index = n - 1; index > n / 2; index--)
 	{
-		tmp = a[n - 1 -index];
+		tmp = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
 		a[index] = tmp;
 	}
