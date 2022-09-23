039909d7594b5ea2399a072301aba484fb7564d2
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-23 09:34:26 -0700

    fux git

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == s2))
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
@@ -23,4 +23,3 @@ int _strcmp(char *s1, char *s2)
 	{
 		return (*s1 - *s2);
 	}
-}
