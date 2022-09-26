b15fe553b94f97371b8d1065bf6f91c154c0fb0f
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-26 07:08:25 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 	/* Declaring FOR */
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) + b; /* add 1 position s */
+		*(s + i) = b; /* add 1 position s */
 	} /* END FOR */
 
 	return (s);
