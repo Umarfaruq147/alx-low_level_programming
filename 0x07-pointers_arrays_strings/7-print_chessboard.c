b1027ba936a294cf3c5a19c4b13d0ebd444202a9
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-26 08:46:29 -0700

    fix 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -16,10 +16,10 @@ void print_chessboard(char (*a)[8])
 		j = 0;
 		while (j < 8)
 		{
-			_putchar (a[i][j]);
+			putchar (a[i][j]);
 			j++;
 		}
-		_putchar ('\n');
+		putchar ('\n');
 		i++;
 	}
 }
