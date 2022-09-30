c67c1e551cb7963d0a4929e0f82382cc171066d2
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-30 15:47:48 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,6 +22,7 @@ int main(int argc, char *argv[])
 	}
 
 	total = atoi(argv[1]);
+
 	if (total <= 0)
 	{
 		printf("0\n");
@@ -35,7 +36,7 @@ int main(int argc, char *argv[])
 		{
 			aux = (total / coins[position]);
 			change += aux;
-			total -= coins[position] = aux;
+			total -= coins[position] * aux;
 		}
 
 		position++;
