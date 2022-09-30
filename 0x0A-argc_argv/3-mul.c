d095b948351cc903204b6189b31c2285439b39f1
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-09-30 15:17:34 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,14 +7,16 @@
  * @argc: Count arguments
  * @argv: Arguments
  * Return: 0 if true, 1 if false
- */int main(int argc, char *argv[])
+ */
+int main(int argc, char *argv[])
 {
 	int a, b;
+
 	if (argc == 3)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
-		printf("%d\n", a *b);
+		printf("%d\n", a * b);
 		return (0);
 	}
 	printf("Error\n");
