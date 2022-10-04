123d4ade655fdba582e19b8cb5144a78978f490a
Umarfaruq147 <adeshinaumarfaruq@gmail.com>
2022-10-04 09:53:18 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 
 	for (a = 48; a < 58; a++)
 	{
-		if (a != 50)
+		if (a != 50 && a !=52)
 		{
-			_putchar(a);
+			putchar(a);
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
